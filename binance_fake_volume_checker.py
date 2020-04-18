0bba378032094e6d39ac79eca07e92c6dba5e4b2
ntom <flibbr@gmail.com>
2020-04-18 16:54:15 +0100

    Added some trade id logging

--- binance_fake_volume_checker.py
+++ binance_fake_volume_checker.py
@@ -10,11 +10,12 @@ last_bid = 0
 last_ask = 0 
 faked_volume = 0
 legit_volume = 0
+last_trade_time = 0 
 
 
 def process_message(msg):
     # os.system('clear')
-    global last_bid, last_ask, faked_volume, legit_volume
+    global last_bid, last_ask, faked_volume, legit_volume, last_trade_time
 
     # Is a order book stream update
     # Since the 'A' key is only present in the book ticker stream
@@ -25,11 +26,22 @@ def process_message(msg):
 
     # Is a trade stream update 
     else:
+        trade_time = msg['T'] 
+
+        # Check this trade time is AFTEr the last trade time
+        # This ensures the trades we're seeing are indeed in order
+        # and not some lagged trade coming through from some post settlment process etc on binances end
+        # This is in miliseconds which does not actually have enough precision 
+        if trade_time < last_trade_time:
+            print('\033[96m', 'ERROR, Trade Time does not follow chronological order', '\033[0m')
+
+        # Check if the trade occured above the last best bid, and below the last best ask
+        # Thus occured 'between the spread' 
         if ( ( msg['p'] > last_bid ) and (msg['p'] < last_ask) ):
-            print('\033[93m', '-- EXECUTION BETWEEN SPREAD (_o_): ', msg['p'], 'for', msg['q'], '\033[0m')
+            print('\033[93m', '-- EXECUTION BETWEEN SPREAD (_o_): ', msg['T'], '--', msg['p'], 'for', msg['q'], '\033[0m')
             faked_volume = faked_volume + float(msg['q'])
         else:
-            print('\u001b[38;5;244m', 'Legit Trade: ', msg['p'], 'for', msg['q'], '\033[0m')
+            print('\u001b[38;5;244m', '-- Legit Trade: ', msg['T'], '--', msg['p'], 'for', msg['q'], '\033[0m')
             legit_volume = legit_volume + float(msg['q'])
 
         print('\033[95m', 'total fake: ', faked_volume, ' BTC', '\033[0m')
