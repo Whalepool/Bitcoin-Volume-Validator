abc4c7b4098d6a962cbc20ec3b6f4256b1ef0ee4
ntom <flibbr@gmail.com>
2020-04-18 16:10:18 +0100

    initial commit

---
+++ binance_fake_volume_checker.py
@@ -0,0 +1,46 @@
+from binance.client import Client
+client = Client("DdEn5fpqEAekNJCVYmflOY9pLfIozLTjU4qXm5HqB9vyFh9PAB7LNdBWPjrkHf43", "c5NjpuJPGYgQZ60ZUXE3z6FQTV3YVOTNVz5JZ5aW2ST3uTJM65mykaFjCDMhPa1D")
+
+from binance.websockets import BinanceSocketManager
+import time
+from pprint import pprint
+import os
+
+last_bid = 0
+last_ask = 0 
+
+
+def process_message(msg):
+    # os.system('clear')
+    global last_bid, last_ask
+
+    # Is a order book stream update
+    # Since the 'A' key is only present in the book ticker stream
+    if 'A' in msg:
+        last_bid = msg['b']
+        last_ask = msg['a']
+        print('\u001b[38;5;83m', last_bid, '\033[0m', '\u001b[38;5;244m', '----', '\033[0m', '\u001b[38;5;196m', last_ask, '\033[0m')
+
+    # Is a trade stream update 
+    else:
+        if ( ( msg['p'] > last_bid ) and (msg['p'] < last_ask) ):
+            print('\033[93m', '-- EXECTION BETWEEN SPREAD: ', msg['p'], 'for', msg['q'], '\033[0m')
+        if ( msg['p'] < last_bid ):
+            print('\u001b[38;5;244m', 'Legit Trade: ', msg['p'], 'for', msg['q'], '\033[0m')
+
+
+bm = BinanceSocketManager(client)
+
+# Real time book ticker stream
+# https://github.com/binance-exchange/binance-official-api-docs/blob/master/web-socket-streams.md#individual-symbol-book-ticker-streams
+# https://github.com/sammchardy/python-binance/blob/c66695a785e8d3cf0975d09b624f79772dac4115/binance/websockets.py#L409
+bm.start_symbol_book_ticker_socket('BTCUSDT', process_message)
+
+# Real time trade execution feed
+# https://github.com/binance-exchange/binance-official-api-docs/blob/master/web-socket-streams.md#trade-streams
+# https://github.com/sammchardy/python-binance/blob/c66695a785e8d3cf0975d09b624f79772dac4115/binance/websockets.py#L254
+bm.start_trade_socket('BTCUSDT', process_message)
+bm.start()
+
+
+
---
+++ readme.md
@@ -0,0 +1,7 @@
+## Binance Fake Volume Checker
+
+```
+pip install -r requirements.txt
+python binance_fake_volume_checker.py
+```
+
