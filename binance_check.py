69537340ce07f4c5d258dff37470fa74336eb7ef
ntom <flibbr@gmail.com>
2020-04-19 15:42:44 +0100

    improvements to nps

--- binance_check.py
+++ binance_check.py
@@ -5,67 +5,110 @@ from binance.websockets import BinanceSocketManager
 import time
 from pprint import pprint
 import os
-
-last_bid = 0
-last_ask = 0 
-faked_volume = 0
-legit_volume = 0
-last_trade_time = 0 
-num_fake_trades = 0
-num_legit_trades = 0
-
-def process_message(msg):
-    # os.system('clear')
-    global last_bid, last_ask, faked_volume, legit_volume, last_trade_time, num_fake_trades, num_legit_trades
-
-    # Is a order book stream update
-    # Since the 'A' key is only present in the book ticker stream
-    if 'A' in msg:
-        last_bid = msg['b']
-        last_ask = msg['a']
-        print('\u001b[38;5;83m', last_bid, '\033[0m', '\u001b[38;5;244m', '----', '\033[0m', '\u001b[38;5;196m', last_ask, '\033[0m')
-
-    # Is a trade stream update 
-    else:
-        trade_time = msg['T'] 
-
-        # Check this trade time is AFTEr the last trade time
-        # This ensures the trades we're seeing are indeed in order
-        # and not some lagged trade coming through from some post settlment process etc on binances end
-        # This is in miliseconds which does not actually have enough precision 
-        if trade_time < last_trade_time:
-            print('\033[96m', 'ERROR, Trade Time does not follow chronological order', '\033[0m')
-
-        # Check if the trade occured above the last best bid, and below the last best ask
-        # Thus occured 'between the spread' 
-        if ( ( msg['p'] > last_bid ) and (msg['p'] < last_ask) ):
-            print('\033[93m', '-- EXECUTION BETWEEN SPREAD (_o_): ', msg['T'], '--', msg['p'], 'for', msg['q'], '\033[0m')
-            faked_volume = faked_volume + float(msg['q'])
-            num_fake_trades = num_fake_trades + 1
-        else:
-            print('\u001b[38;5;244m', '-- Legit Trade: ', msg['T'], '--', msg['p'], 'for', msg['q'], '\033[0m')
-            legit_volume = legit_volume + float(msg['q'])
-            num_legit_trades = num_legit_trades + 1
-
-        print('\033[95m', 'total fake volume: ', faked_volume, ' BTC', '\033[0m')
-        print('\033[95m', 'total legit volume: ', legit_volume, ' BTC', '\033[0m')
-        print('\033[95m', 'number of fake trades: ', num_fake_trades, '\033[0m')
-        print('\033[95m', 'number of legit trades: ', num_legit_trades, '\033[0m')
+import zmq
+import json  
 
 
-
-bm = BinanceSocketManager(client)
-
-# Real time book ticker stream
-# https://github.com/binance-exchange/binance-official-api-docs/blob/master/web-socket-streams.md#individual-symbol-book-ticker-streams
-# https://github.com/sammchardy/python-binance/blob/c66695a785e8d3cf0975d09b624f79772dac4115/binance/websockets.py#L409
-bm.start_symbol_book_ticker_socket('BTCUSDT', process_message)
-
-# Real time trade execution feed
-# https://github.com/binance-exchange/binance-official-api-docs/blob/master/web-socket-streams.md#trade-streams
-# https://github.com/sammchardy/python-binance/blob/c66695a785e8d3cf0975d09b624f79772dac4115/binance/websockets.py#L254
-bm.start_trade_socket('BTCUSDT', process_message)
-bm.start()
+def mogrify(topic, msg):
+    """ json encode the message and prepend the topic """
+    return topic + ' ' + json.dumps(msg,  default=str)
 
 
+class BinanceChecker: 
 
+    def __init__(self):
+
+        self.context = zmq.Context()
+        self.consumer_sender = self.context.socket(zmq.PUB)
+        self.consumer_sender.connect('tcp://127.0.0.1:5000')
+
+        self.last_bid = 0
+        self.last_ask = 0 
+        self.faked_volume = 0
+        self.legit_volume = 0
+        self.last_trade_time = 0 
+        self.num_fake_trades = 0
+        self.num_legit_trades = 0
+
+    def send_to_zmq(self, data):
+        data = mogrify('binance_output', data)
+        self.consumer_sender.send_string(data, zmq.NOBLOCK)
+
+
+    def run(self):
+
+        def process_message(msg):
+            # os.system('clear')
+            # global last_bid, last_ask, faked_volume, legit_volume, last_trade_time, num_fake_trades, num_legit_trades
+
+            # pprint(last_trade_time)
+            # os._exit(1)
+
+            # Is a order book stream update
+            # Since the 'A' key is only present in the book ticker stream
+            if 'A' in msg:
+                self.last_bid = msg['b']
+                self.last_ask = msg['a']
+                self.send_to_zmq( {'action': 'book_update', 'last_bid':self.last_bid, 'last_ask': self.last_ask} )
+                print('\u001b[38;5;83m', self.last_bid, '\033[0m', '\u001b[38;5;244m', '----', '\033[0m', '\u001b[38;5;196m', self.last_ask, '\033[0m')
+
+            # Is a trade stream update 
+            else:
+                trade_time = msg['T'] 
+
+                # Check this trade time is AFTEr the last trade time
+                # This ensures the trades we're seeing are indeed in order
+                # and not some lagged trade coming through from some post settlment process etc on binances end
+                # This is in miliseconds which does not actually have enough precision 
+                if trade_time < self.last_trade_time:
+                    self.send_to_zmq({'action': 'error', 'msg': 'ERROR, Trade Time does not follow chronological order'})
+                    print('\033[96m', 'ERROR, Trade Time does not follow chronological order', '\033[0m')
+
+                # Check if the trade occured above the last best bid, and below the last best ask
+                # Thus occured 'between the spread' 
+                if ( ( msg['p'] > self.last_bid ) and (msg['p'] < self.last_ask) ):
+                    self.send_to_zmq({'action': 'order_mismatch', 'msg': '-- EXECUTION BETWEEN SPREAD (_o_): '+str(msg['T'])+'--'+str(msg['p'])+' for '+str(msg['q'])})
+                    print('\033[93m', '-- EXECUTION BETWEEN SPREAD (_o_): ', msg['T'], '--', msg['p'], 'for', msg['q'], '\033[0m')
+
+                    self.faked_volume = self.faked_volume + float(msg['q'])
+                    self.num_fake_trades = self.num_fake_trades + 1
+                else:
+                    self.send_to_zmq({'action': 'order_legit', 'msg': '-- Legit Trade: '+str(msg['T'])+'--'+str(msg['p'])+' for '+str(msg['q'])})
+                    print('\u001b[38;5;244m', '-- Legit Trade: ', msg['T'], '--', msg['p'], 'for', msg['q'], '\033[0m')
+
+                    self.legit_volume = self.legit_volume + float(msg['q'])
+                    self.num_legit_trades = self.num_legit_trades + 1
+
+
+                self.send_to_zmq({'action': 'summary', 
+                    'total_trades': (self.num_fake_trades+self.num_legit_trades),
+                    'sum_fake_volume': self.faked_volume,
+                    'sum_legit_volume': self.legit_volume,
+                    'sum_fake_trades': self.num_fake_trades,
+                    'sum_legit_trades': self.num_legit_trades,
+                })
+                print('\033[95m', 'total fake volume: ', self.faked_volume, ' BTC', '\033[0m')
+                print('\033[95m', 'total legit volume: ', self.legit_volume, ' BTC', '\033[0m')
+                print('\033[95m', 'number of fake trades: ', self.num_fake_trades, '\033[0m')
+                print('\033[95m', 'number of legit trades: ', self.num_legit_trades, '\033[0m')
+
+
+
+        bm = BinanceSocketManager(client)
+
+        # Real time book ticker stream
+        # https://github.com/binance-exchange/binance-official-api-docs/blob/master/web-socket-streams.md#individual-symbol-book-ticker-streams
+        # https://github.com/sammchardy/python-binance/blob/c66695a785e8d3cf0975d09b624f79772dac4115/binance/websockets.py#L409
+        bm.start_symbol_book_ticker_socket('BTCUSDT', process_message)
+
+        # Real time trade execution feed
+        # https://github.com/binance-exchange/binance-official-api-docs/blob/master/web-socket-streams.md#trade-streams
+        # https://github.com/sammchardy/python-binance/blob/c66695a785e8d3cf0975d09b624f79772dac4115/binance/websockets.py#L254
+        bm.start_trade_socket('BTCUSDT', process_message)
+        bm.start()
+
+
+
+if __name__ == "__main__":
+    App = BinanceChecker()
+    App.run()
--- nps.py
+++ nps.py
@@ -1,38 +1,241 @@
 
-import npyscreen
-import curses
+from binance_check import BinanceChecker
+import zmq
+import json
+import argparse
+from pprint import pprint
 
-#npyscreen.disableColor()
-class TestApp(npyscreen.NPSApp):
-    def main(self):
-        #self.test_memory_use()
-        self.test()
-    
-    def test(self):
-        # These lines create the form and populate it with widgets.
-        # A fairly complex screen in only 8 or so lines of code - a line for each control.
-        F  = npyscreen.ActionFormV2WithMenus(name = "Welcome to Npyscreen",)
+parser = argparse.ArgumentParser()
+parser.add_argument('-e','--exchanges', nargs='+', help='Exchanges', required=True, default=[])
+args = parser.parse_args()
 
-        ms = F.add(npyscreen.MultiLineActionWithShortcuts, max_height=4, value = [1,], name="Pick One", 
-                values = ["Option1","Option2","Option3"], scroll_exit=True)
-        
-        root = F.new_menu()
-        root.addItem('test', curses.beep)
-        
-        # This lets the user play with the Form.
-        F.edit()
+allowed_exchanges = {
+    'binance': { 'name': 'Binance' },
+    'bitstamp': { 'name': 'Bitstamp' },
+    'coinbase': { 'name': 'Coinbase' },
+    'ftx': { 'name': 'FTX' },
+    'kraken': { 'name': 'Kraken' }
+}
 
-    def test_memory_use(self):
-        n = 0 
-        while 1:
-            F  = npyscreen.fmActionFormV2.ActionFormV2(name = "Welcome to Npyscreen %s" % n)
-            #F  = npyscreen.ActionForm(name = "Welcome to Npyscreen %s" % n)
-            
-            ms = F.add(npyscreen.MultiLineActionWithShortcuts, max_height=4, value = [1,], name="Pick One", 
-                    values = ["Option1","Option2","Option3"], scroll_exit=True)
-        
-            F.display()
-            n +=1
-if __name__ == "__main__":
-    App = TestApp()
-    App.run()   
+for e in args.exchanges:
+    if e not in allowed_exchanges:
+        msg = e+' is an invalid exchange, allowed are: '+str(list(allowed_exchanges.keys()))
+        raise SystemExit(msg)
+
+
+def demogrify(topicmsg):
+    """ Inverse of mogrify() """
+    json0 = topicmsg.find('[')
+    json1 = topicmsg.find('{')
+
+    start = json0
+    if (json1 > 0):
+        if (json0 > 0):
+            if (json1 < json0):
+                start = json1
+        else:
+            start = json1
+
+    topic = topicmsg[0:start].strip()
+    msg = json.loads(topicmsg[start:])
+
+    return topic, msg   #
+
+
+from prompt_toolkit.application import Application
+from prompt_toolkit.document import Document
+from prompt_toolkit.buffer import Buffer
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.layout.containers import HSplit, VSplit, Window, WindowAlign, ScrollOffsets
+from prompt_toolkit.layout.controls import BufferControl, FormattedTextControl
+from prompt_toolkit.layout.layout import Layout
+from prompt_toolkit.layout.dimension import LayoutDimension as D
+from prompt_toolkit.styles import Style
+from prompt_toolkit.widgets import TextArea
+from prompt_toolkit.layout.processors import Processor, Transformation
+from prompt_toolkit.formatted_text import to_formatted_text, fragment_list_to_text
+from prompt_toolkit.layout.margins import Margin, NumberedMargin, ScrollbarMargin
+from prompt_toolkit.formatted_text import HTML, FormattedText
+from prompt_toolkit import ANSI
+
+
+
+
+# The key bindings.
+kb = KeyBindings()
+
+@kb.add('c-c')
+@kb.add('c-q')
+def _(event):
+    event.app.exit()
+
+
+style = Style([
+    ('output-field', 'bg:#000044 #ffffff'),
+    ('maintitle', '#aaaa00'),
+    ('maintitle.since', 'reverse'),
+    ('vline', 'reverse'),
+    ('mbline', "fg:ansiyellow bg:black bold"),
+    ('lastbid', "#34eb37"),
+    ('spacer', "#bfbfbf bold"),
+    ('lastask', "#eb4c34"),
+    ('status', "#aaaa00"),
+    ('status.position', '#aaaa00'),
+    ('status.key', '#87992f'),
+])
+
+
+
+def get_big_text(): 
+    return [ 
+        ("class:maintitle", 'Volume Monitor running for '),
+        ("class:maintitle.since", ' 4 hours '),
+    ]
+
+def get_summary_text(data=None): 
+    total_trades = 0
+    sum_fake_volume = 0
+    sum_legit_volume = 0
+    sum_fake_trades = 0
+    sum_legit_trades = 0
+    if data is not None:
+        total_trades = data['total_trades']
+        sum_fake_volume = data['sum_fake_volume']
+        sum_legit_volume = data['sum_legit_volume']
+        sum_fake_trades = data['sum_fake_trades']
+        sum_legit_trades = data['sum_legit_trades']
+
+    return [ 
+        ("class:status", 'Total Trades: '),
+        ("class:status.key", " "+str(total_trades)),
+        ("class:status", '\n'),
+        ("class:status", "No. of Fake trades: "),
+        ("class:status.key", " "+str(sum_fake_trades)+" "),
+        ("class:status", '\n'),
+        ("class:status", "No. of Legit trades: "),
+        ("class:status.key", " "+str(sum_legit_trades)+" "),
+        ("class:status", '\n'),
+        ("class:status", "Fake volume: "),
+        ("class:status.key", " "+str(sum_fake_volume)+" BTC"),
+        ("class:status", '\n'),
+        ("class:status", "Legit volume: "),
+        ("class:status.key", " "+str(sum_legit_volume)+" BTC"),
+    ]
+
+
+
+
+def make_exchange_container( exchange ): 
+    title = FormattedTextControl([ ("class:status", ' '+exchange+' '), ("class:status.key", " "), ])
+    exchange = {} 
+    exchange['title'] = title
+    # exchange['book_buffer'] = FormattedTextControl()
+    exchange['book_buffer'] = Buffer()
+    exchange['trades_buffer'] = Buffer()
+    exchange['summary'] = FormattedTextControl()
+    exchange['summary'].text = get_summary_text()
+    exchange['container'] = HSplit(
+        [
+            # The titlebar.
+            Window( height=1, content=exchange['title'], align=WindowAlign.LEFT, left_margins=[ScrollbarMargin()], ),
+            # Horizontal separator.
+            Window(height=1, char="-", style="class:line"),
+            # The 'body', like defined above.
+            VSplit([
+                Window(height=5, width=45, content=exchange['summary'], align=WindowAlign.LEFT, left_margins=[ScrollbarMargin()], ),
+                Window(width=1, char=".", style="class:mbline"),
+                Window(BufferControl(buffer=exchange['book_buffer']), height=5),
+                # Window(height=6, content=exchange['book_buffer']),
+            ]),
+            Window(height=1, char="-", style="class:line"),
+            Window(BufferControl(buffer=exchange['trades_buffer'])),
+        ]
+    )
+    return exchange
+
+exchanges = {}
+for e in args.exchanges:
+    exchanges[e] = make_exchange_container( allowed_exchanges[e]['name'] )
+
+
+def get_vsplit():
+    out = []
+    for i,e in enumerate(args.exchanges):
+        out.append(exchanges[e]['container'])
+        i += 1
+        if i == len(args.exchanges):
+            continue 
+        out.append(Window(width=1, char="|", style="class:vline"))
+
+    return out
+
+root_root = HSplit([
+        # Window( height=1, content=get_big_text(), align=WindowAlign.CENTER ),
+        Window( height=1, content=FormattedTextControl(get_big_text), align=WindowAlign.CENTER ),
+        Window(height=1, char="-", style="class:line"),
+        VSplit(get_vsplit())
+    ])
+
+# Run application.
+application = Application(
+    layout=Layout(root_root),
+    key_bindings=kb,
+    style=style,
+    mouse_support=True,
+    full_screen=True)
+
+import os
+def getmzq():
+
+    global exchanges
+
+    # output_text = output_field.text + formatted_in+formatted_out
+
+    #     # Add text to output buffer.
+    #     output_field.buffer.document = Document(text=output_text, cursor_position=len(output_text))
+
+
+    context = zmq.Context()
+    consumer_receiver = context.socket(zmq.SUB)
+    consumer_receiver.bind('tcp://127.0.0.1:5000')
+    for exchange_key,exchange_layout in exchanges.items():
+        consumer_receiver.setsockopt_string(zmq.SUBSCRIBE, exchange_key+'_output' )
+
+    while True:
+        msg = consumer_receiver.recv()
+        topic, trade = demogrify(msg.decode("utf-8"))
+        exchange_key = topic.split('_')[0]
+
+        if trade['action'] == 'book_update':
+            # exchanges[exchange_key]['book_buffer'].insert_text(str(trade)+'\n')
+            text = [ 
+                ("class:lastbid", ' '+str(trade['last_bid'])+' '), 
+                ("class:spacer", ' ------- '),
+                ("class:lastask", ' '+str(trade['last_ask'])+' '),
+            ]
+            exchanges[exchange_key]['book_buffer'].insert_text( str(trade['last_bid'])+' ------- '+str(trade['last_ask'])+'\n' )
+            # exchanges[exchange_key]['book_buffer'].text = exchanges[exchange_key]['book_buffer'].text + text
+
+        if trade['action'] == 'summary':
+            exchanges[exchange_key]['summary'].text = get_summary_text(data=trade)
+
+        if (trade['action'] == 'order_mismatch') or (trade['action'] == 'order_legit'):
+            exchanges[exchange_key]['trades_buffer'].insert_text(str(trade)+'\n')
+        # output_text = output_field.text + str(trade)
+        # output_field.buffer.document = Document(text=output_text, cursor_position=len(output_text))
+        # exchanges['binance']['title'].text = str(trade)
+
+import threading 
+t = threading.Thread(target=getmzq)
+t.daemon = True
+t.start()
+
+
+application.run()
+
+
+
+
+
+
+
