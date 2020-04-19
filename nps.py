390a8c6288464c9b1538fd608d74f0c0bb29e2a8
ntom <flibbr@gmail.com>
2020-04-19 11:18:41 +0100

    q:

--- nps.py
+++ nps.py
@@ -1,25 +1,38 @@
 
-#!/usr/bin/env python
 import npyscreen
+import curses
 
-class TestListClass(npyscreen.MultiLine):
-    _contained_widgets = npyscreen.AnnotateTextboxBase
-
-class TestForm(npyscreen.FormMutt):
-    MAIN_WIDGET_CLASS = TestListClass
-
+#npyscreen.disableColor()
 class TestApp(npyscreen.NPSApp):
     def main(self):
-        F = TestForm()
-        F.wStatus1.value = "Status Line "
-        F.wStatus2.value = "Second Status Line "
-        F.wMain.values   = [str(x) for x in range(500)]
+        #self.test_memory_use()
+        self.test()
+    
+    def test(self):
+        # These lines create the form and populate it with widgets.
+        # A fairly complex screen in only 8 or so lines of code - a line for each control.
+        F  = npyscreen.ActionFormV2WithMenus(name = "Welcome to Npyscreen",)
 
-        F.wMain.values.insert(0, 'test first line')
+        ms = F.add(npyscreen.MultiLineActionWithShortcuts, max_height=4, value = [1,], name="Pick One", 
+                values = ["Option1","Option2","Option3"], scroll_exit=True)
         
+        root = F.new_menu()
+        root.addItem('test', curses.beep)
+        
+        # This lets the user play with the Form.
         F.edit()
 
-
+    def test_memory_use(self):
+        n = 0 
+        while 1:
+            F  = npyscreen.fmActionFormV2.ActionFormV2(name = "Welcome to Npyscreen %s" % n)
+            #F  = npyscreen.ActionForm(name = "Welcome to Npyscreen %s" % n)
+            
+            ms = F.add(npyscreen.MultiLineActionWithShortcuts, max_height=4, value = [1,], name="Pick One", 
+                    values = ["Option1","Option2","Option3"], scroll_exit=True)
+        
+            F.display()
+            n +=1
 if __name__ == "__main__":
     App = TestApp()
-    App.run()
+    App.run()   
